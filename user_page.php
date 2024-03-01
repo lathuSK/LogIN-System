b4ce325a511d6bedda87bc51d1a336511ce6a108
lathusansiva@gmail.com <youremail@example.com>
2024-03-01 10:29:54 +0530

    New Commit

---
+++ user_page.php
@@ -0,0 +1,54 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(!isset($_SESSION['user_name'])){
+   header('location:login_form.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>user page</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<div class="container">
+
+   <div class="content">
+      <h3>hi, <span>user</span></h3>
+      <h1>welcome <span><?php echo $_SESSION['user_name'] ?></span></h1>
+      <p>this is an user page</p>
+      <a href="login_form.php" class="btn">login</a>
+      <a href="register_form.php" class="btn">register</a>
+      <a href="logout.php" class="btn">logout</a>
+      <div class="form-container-two">
+         <form action="" method="post">
+            <h3>Booking now</h3>
+            <?php
+               if(isset($error)){
+                  foreach($error as $error){
+                     echo '<span class="error-msg">'.$error.'</span>';
+                  };
+               };
+            ?>
+            <input type="text" name="name" required placeholder="enter your name">
+            <input type="email" name="email" required placeholder="enter your email">
+            <input type="submit" name="submit" value="Booking now" class="form-btn">
+         </form>
+      </div>
+   </div>
+
+</body>
+</html>
